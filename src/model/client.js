62f7d6770d0e8a5b0b01b2fd6c34ff794965ef7a
alenca <80914326+naikspa@users.noreply.github.com>
2023-03-02 16:44:26 -0300

    Add files via upload

--- src/model/client.js
+++ src/model/client.js
@@ -1,15 +1,6 @@
 const mongoose = require("mongoose");
 const { Schema, model } = require("mongoose");
 const cli = require("nodemon/lib/cli");
-const url =
-  "mongodb+srv://Enzo:UDRDRiI6l0NMmj0G@arthur-workshop.bwnuexp.mongodb.net/database-aw?retryWrites=true&w=majority";
-
-mongoose
-  .connect(url)
-  .then(() => console.log("SE HA CONECTADO A MONGODB"))
-  .catch((e) =>
-    console.error("No se conecto a MONGODB debido a este error:" + e)
-  );
 
 const clientSchema = new Schema(
   {
